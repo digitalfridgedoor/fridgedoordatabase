6f75f182e56073c17984072ddf7ec4c7e431ae20
Maisie <maisiesadler@hotmail.com>
2019-12-12 08:04:22 +0000

    Add new ingredient

---
+++ ingredient/create.go
@@ -0,0 +1,29 @@
+package ingredient
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Create creates a new ingredient with given name
+func (coll *Collection) Create(ctx context.Context, userID primitive.ObjectID, name string) (*Ingredient, error) {
+
+	insertOneOptions := options.InsertOne()
+
+	ingredient := &Ingredient{
+		Name:    name,
+		AddedOn: time.Now(),
+	}
+
+	insertOneResult, err := coll.mongoCollection().InsertOne(ctx, ingredient, insertOneOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	insertedID := insertOneResult.InsertedID.(primitive.ObjectID)
+
+	return coll.FindOne(ctx, insertedID.Hex())
+}
