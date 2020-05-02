f8bac89ab1d640e43bd5ac31ca322901a82c86b6
Maisie <maisiesadler@hotmail.com>
2020-05-02 15:50:10 +0100

    id as *

--- dfdmodels/userview.go
+++ dfdmodels/userview.go
@@ -6,8 +6,8 @@ import (
 
 // UserView represents a users set of lists
 type UserView struct {
-	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Username string             `json:"username"`
-	Nickname string             `json:"nickname"`
-	Tags     []string           `json:"tags"`
+	ID       *primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Username string              `json:"username"`
+	Nickname string              `json:"nickname"`
+	Tags     []string            `json:"tags"`
 }
