2d24afa14f08aee98f5bd46e3dacbdb6de2d241f
Maisie <maisiesadler@hotmail.com>
2020-05-03 15:48:43 +0100

    findRecipeByTagsTestPredicate

--- dfdtesting/findpredicates.go
+++ dfdtesting/findpredicates.go
@@ -1,6 +1,7 @@
 package dfdtesting
 
 import (
+	"fmt"
 	"regexp"
 
 	"github.com/digitalfridgedoor/fridgedoordatabase/dfdmodels"
@@ -39,6 +40,11 @@ func SetRecipeFindByNamePredicate() {
 	SetRecipeFindPredicate(findRecipeByNameTestPredicate)
 }
 
+// SetRecipeFindByTagsPredicate overrides logic for find recipe in FindByTags method
+func SetRecipeFindByTagsPredicate() {
+	SetRecipeFindPredicate(findRecipeByTagsTestPredicate)
+}
+
 func findRecipeByNameTestPredicate(gs *dfdmodels.Recipe, m primitive.M) bool {
 
 	andval := m["$and"].([]bson.M)
@@ -54,4 +60,59 @@ func findRecipeByNameTestPredicate(gs *dfdmodels.Recipe, m primitive.M) bool {
 	r := regexp.MustCompile(regexval.Pattern)
 
 	return r.MatchString(gs.Name)
+}
+
+func findRecipeByTagsTestPredicate(r *dfdmodels.Recipe, m bson.M) bool {
+
+	contains := func(tags []string, tag string) bool {
+		for _, t := range tags {
+			if t == tag {
+				return true
+			}
+		}
+		return false
+	}
+
+	andval := m["$and"].([]bson.M)
+	addedby := (andval)[0]["addedby"].(primitive.ObjectID)
+
+	if addedby != r.AddedBy {
+		return false
+	}
+
+	if len(andval) > 1 {
+		t := andval[1]["metadata.tags"].(bson.M)
+		if all, ok := t["$all"]; ok {
+			tags := all.([]string)
+			for _, t := range tags {
+				if !contains(r.Metadata.Tags, t) {
+					return false
+				}
+			}
+		} else if all, ok := t["$nin"]; ok {
+			tags := all.([]string)
+			for _, t := range tags {
+				if contains(r.Metadata.Tags, t) {
+					return false
+				}
+			}
+		} else {
+			fmt.Println("unexpected value")
+			return false
+		}
+	}
+
+	if len(andval) > 2 {
+		t := andval[2]["metadata.tags"].(bson.M)
+		if all, ok := t["$nin"]; ok {
+			tags := all.([]string)
+			for _, t := range tags {
+				if contains(r.Metadata.Tags, t) {
+					return false
+				}
+			}
+		}
+	}
+
+	return true
 }
