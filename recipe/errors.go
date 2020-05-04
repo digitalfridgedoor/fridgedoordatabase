a8dfd0d048da08a328fa258c1c06281a7ef3225f
Maisie <maisiesadler@hotmail.com>
2020-05-04 19:51:20 +0100

    remove recipepackage

--- recipe/errors.go
+++
@@ -1,8 +0,0 @@
-package recipe
-
-import "errors"
-
-var errNotConnected = errors.New("Not connected")
-var errDuplicate = errors.New("Duplicate")
-var errSubRecipe = errors.New("Invalid subrecipe reference")
-var errUnauthorised = errors.New("Cannot perform requested action on this recipe")
