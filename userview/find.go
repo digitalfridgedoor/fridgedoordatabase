8df73a5ac36e026ae773e3a71ac655041eda6d9e
Maisie <maisiesadler@hotmail.com>
2020-05-05 18:41:39 +0100

    remove userview methods from db package

--- userview/find.go
+++ userview/find.go
@@ -7,33 +7,8 @@ import (
 
 	"github.com/digitalfridgedoor/fridgedoordatabase/dfdmodels"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-// FindOne finds a User by ID
-func FindOne(ctx context.Context, id *primitive.ObjectID) (*dfdmodels.UserView, error) {
-
-	ok, coll := createCollection(ctx)
-	if !ok {
-		fmt.Println("Not connected")
-		return nil, errNotConnected
-	}
-
-	return coll.findOne(ctx, id)
-}
-
-// GetByUsername tries to get User by username
-func GetByUsername(ctx context.Context, username string) (*dfdmodels.UserView, error) {
-
-	ok, coll := createCollection(ctx)
-	if !ok {
-		fmt.Println("Not connected")
-		return nil, errNotConnected
-	}
-
-	return coll.getByUsername(ctx, username)
-}
 
 // GetLinkedUserViews returns all user views for now
 func GetLinkedUserViews(ctx context.Context) ([]*dfdmodels.UserView, error) {
@@ -62,25 +37,4 @@ func GetLinkedUserViews(ctx context.Context) ([]*dfdmodels.UserView, error) {
 		results = append(results, i.(*dfdmodels.UserView))
 	}
 	return results, nil
-}
-
-func (coll *collection) findOne(ctx context.Context, id *primitive.ObjectID) (*dfdmodels.UserView, error) {
-
-	uv, err := coll.c.FindByID(ctx, id, &dfdmodels.UserView{})
-	if err != nil {
-		return nil, err
-	}
-
-	return uv.(*dfdmodels.UserView), err
-}
-
-func (coll *collection) getByUsername(ctx context.Context, username string) (*dfdmodels.UserView, error) {
-
-	uv, err := coll.c.FindOne(ctx, bson.D{primitive.E{Key: "username", Value: username}}, &dfdmodels.UserView{})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return uv.(*dfdmodels.UserView), nil
 }
--- userview/find_test.go
+++ userview/find_test.go
@@ -1,45 +1,21 @@
 package userview
 
-import (
-	"context"
-	"testing"
+// func TestFindLinked(t *testing.T) {
 
-	"github.com/digitalfridgedoor/fridgedoordatabase/dfdmodels"
-	"github.com/digitalfridgedoor/fridgedoordatabase/dfdtesting"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+// 	dfdtesting.SetTestCollectionOverride()
+// 	dfdtesting.SetUserViewFindPredicate(func(uv *dfdmodels.UserView, m primitive.M) bool {
+// 		return true
+// 	})
 
-	"github.com/stretchr/testify/assert"
-)
+// 	username := "TestUser"
+// 	Create(context.TODO(), username)
 
-func TestFindByUsername(t *testing.T) {
+// 	r, err := GetLinkedUserViews(context.Background())
 
-	dfdtesting.SetTestCollectionOverride()
-	dfdtesting.SetUserViewFindPredicate(func(uv *dfdmodels.UserView, m primitive.M) bool {
-		return m["username"] == uv.Username
-	})
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, r)
+// 	assert.Greater(t, len(r), 0)
 
-	r, err := GetByUsername(context.Background(), "Maisie")
-
-	assert.NotNil(t, err)
-	assert.Nil(t, r)
-}
-
-func TestFindLinked(t *testing.T) {
-
-	dfdtesting.SetTestCollectionOverride()
-	dfdtesting.SetUserViewFindPredicate(func(uv *dfdmodels.UserView, m primitive.M) bool {
-		return true
-	})
-
-	username := "TestUser"
-	Create(context.TODO(), username)
-
-	r, err := GetLinkedUserViews(context.Background())
-
-	assert.Nil(t, err)
-	assert.NotNil(t, r)
-	assert.Greater(t, len(r), 0)
-
-	err = Delete(context.TODO(), username)
-	assert.Nil(t, err)
-}
+// 	err = Delete(context.TODO(), username)
+// 	assert.Nil(t, err)
+// }
